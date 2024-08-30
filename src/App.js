87ecdec0bfb31e310c0185e0d1fee5b5256dd7e7
codewithswatiee <verma.s@somaiya.edu>
2024-08-30 06:06:40 +0530

    created action creators

--- src/App.js
+++ src/App.js
@@ -2,23 +2,37 @@ import { useEffect } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import Notification from './components/UI/Notification';
+import { sendCartData } from './store/cart-slice';
+
+let isInitial = true;
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector(state => state.ui.cartIsVisible)
   const cart = useSelector(state => state.cart)
+  const notification = useSelector(state => state.ui.notification)
 
   useEffect(() => {
-    fetch('https://cart-a662c-default-rtdb.firebaseio.com/cart.json', {
-      method: 'PUT',
-      body: JSON.stringify(cart),
-    })
-  }, [cart]);
+    if(isInitial){
+      isInitial = false;
+      return;
+    }
+    dispatch(sendCartData(cart));
+  }, [cart, dispatch]);
   return (
-    <Layout>
+    <>
+      {notification && <Notification 
+      status={notification.status}
+      title={notification.title}
+      message={notification.message}
+      />}
+      <Layout>
       {showCart && <Cart />}
       <Products />
     </Layout>
+    </>
   );
 }
 
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./ui-slice";
 
 const cartSlice = createSlice({
     name: 'cart',
@@ -40,6 +41,42 @@ const cartSlice = createSlice({
 
     }
 })
+
+
+export const sendCartData = (cart) => {
+    return async (dispatch) => {
+        dispatch(uiActions.showNotification({
+            status: 'pending',
+            title: 'Loading...',
+            message: 'Please wait'
+          }));
+
+          const senRequest = async () =>{
+            const response = await fetch('https://cart-a662c-default-rtdb.firebaseio.com/cart.json', {
+                method: 'PUT',
+                body: JSON.stringify(cart),
+              });
+    
+              if(!response){
+                throw new Error("Sending out data file failed.")
+              }
+          }
+          try{
+            await senRequest();
+            dispatch(uiActions.showNotification({
+                status: 'success',
+                title: 'Success!...',
+                message: 'Sent Successfully!'
+              }))
+            }catch(error){
+                dispatch(uiActions.showNotification({
+                    status: 'error',
+                    title: 'Error!...',
+                    message: 'Sending cart data failed!'
+                  }))
+            }
+        };
+}
 
 export const cartActions = cartSlice.actions;
 export default cartSlice;
